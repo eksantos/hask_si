7df799af0f9fd99747ad00c6e85e1a3b153abf80
Ubuntu <ubuntu@ip-172-31-29-135.ec2.internal>
2019-10-31 23:50:30 +0000

    ARQUIVOS DE PÁGINA

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,10 +10,19 @@ import Import
 import Data.FileEmbed (embedFile)
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Text.Julius
+import Text.Lucius
 
 getAdsR :: Handler TypedContent
 getAdsR = return $ TypedContent "text/plain"
     $ toContent $(embedFile "static/ads.txt")
+    
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page2.hamlet")
+        $(luciusFile "templates/page2.lucius")
+        $(juliusFile "templates/page2.julius")
     
 getPage1R :: Handler Html
 getPage1R = do
