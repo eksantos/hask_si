0e131095528f91b14d42eea448e7d3f30ca057b3
Ubuntu <ubuntu@ip-172-31-29-135.ec2.internal>
2019-11-08 00:24:10 +0000

    FORMULARIO ALUNO

--- src/Handler/Aluno.hs
+++ src/Handler/Aluno.hs
@@ -20,36 +20,38 @@ formAluno = rederBootsrap $ Aluno
     <*> areq textField  "RA: " Nothing
     <*> areq dayField   "Data: " Nothing
 
+-- $maybe é um if 
+-- $nothing é o else 
 
-getAlunoR :: Handler Html
+getAlunoR :: Handler Html 
 getAlunoR = do 
-    (widget, enctype) <- generateFormPost formAluno
+    (widget, enctype) <- generateFormPost formAluno 
     msg <- getMessage
-    defaultLayout $ do
-    addStylesheet (StaticR css_bootstrap_css)
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
         [whamlet|
-            $maybe mensa <- msg --é um if
+            $maybe mensa <- msg --
                 <div>
                     ^{mensa}
-            --$nothing é o else
-                        <h1>
-                            CADASTRO DE ALUNO
-                        <form method = post action=@{AlunoR}>
-                            ^{widget}
-                            <button>
-                                Cadastrar
+            
+            <h1>
+                CADASTRO DE ALUNO
+            
+            <form method=post action=@{AlunoR}>
+                ^{widget}
+                <button>
+                    Cadastrar
         |]
 
-postAlunoR :: Handler Html
-postAlunoR = do
-    ((result, _), _) <- runFormPost formAluno
-    case result of
-        FormSuccess aluno -> do
-            runDB $ insert aluno
-            setMessage[shamlet| -- shamlet é para mensagem no servidor
+postAlunoR :: Handler Html 
+postAlunoR = do 
+    ((result,_),_) <- runFormPost formAluno 
+    case result of 
+        FormSuccess aluno -> do 
+            runDB $ insert aluno 
+            setMessage [shamlet|
                 <h2>
                     REGISTRO INCLUIDO
             |]
             redirect AlunoR
         _ -> redirect HomeR
-        
