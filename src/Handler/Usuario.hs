96607a01ae8b24231f49e4c007c1174e512fb143
Ubuntu <ubuntu@ip-172-31-29-135.ec2.internal>
2019-11-14 23:08:05 +0000

    SSL

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -10,3 +10,31 @@ import Import
 import Database.Persist.Postgresql
 import Text.Julius
 import Text.Lucius
+import Database.Persist.Postgresql
+
+-- renderDivs
+formUsu :: Form (Usuario, Text)
+formUsu = renderBootstrap $ (,)
+    <$> (Usuario 
+        <$> areq textField "Nome: " Nothing
+        <*> areq emailField "E-mail: " Nothing
+        <*> areq passwordField "Senha: " Nothing)
+    <*> areq passwordField "Digite Novamente: " Nothing
+
+getUsuarioR :: Handler Html
+getUsuarioR = do 
+    (widget,_) <- generateFormPost formUsu
+    msg <- getMessage
+    defaultLayout $ 
+        [whamlet|
+            $maybe mensa <- msg 
+                <div>
+                    ^{mensa}
+            
+            <h1>
+                CADASTRO DE USUARIO
+            
+            <form method=post action=@{UsuarioR}>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
+        |]
