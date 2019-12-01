fc713087db9f3afa39da417d1d9056b1690b5f01
Ubuntu <ubuntu@ip-172-31-29-135.ec2.internal>
2019-12-01 18:15:08 +0000

    MUDANCA NO SITE

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -37,10 +37,10 @@ getPage1R = do
 
 getHomeR :: Handler Html
 getHomeR = do 
-    sess <- lookupSession "_NOME"
     defaultLayout $ do 
     -- addScriptRemote "url" -> CHAMA JS EXTERNO
     -- addScript (StaticR script_js) -> JS INTERNO
+        toWidgetHead $(luciusFile "templates/page2.lucius")
         addStylesheet (StaticR css_bootstrap_css)
         toWidgetHead [julius|
         <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -53,131 +53,7 @@ getHomeR = do
             gtag('config', 'UA-72614868-2');
         |]
         toWidgetHead [lucius|
-        @import url('https://fonts.googleapis.com/css?family=Londrina+Solid&display=swap');
-        @import url('https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap');
-        
-        body{
-           
-        }
-        main{
-            background-color: #fbfbfb;
-            display: flex;
-            padding: 50px;
-            box-shadow: 0 0 1em #222831;
-        }
-        header h1{
-            font-size: 3em;
-            color: black;
-            font-family: 'Luckiest Guy', cursive;
-            text-align: center;
-        }
-        .divFlexs{
-            flex: 3;
-            margin: 2%;
-            height: 35rem;
-            border: 2px solid #61f2f5;
-            text-align: center;
-            box-shadow: 0 0 1em #c8cfd9;
-        }
-        
-        .divFlexs:hover{
-            border: 2px dashed #61f2f5;
-        }
-        
-        a.btns{
-            padding: 3% 5%;
-            display: inline-block;
-            color: white;
-            font-size: 1.5em;
-            text-decoration: none;
-            margin-top: 99%;
-            background-color: #5c149b;
-            box-shadow: 0 0 1em #222831;
-            font-family: 'Londrina Solid', cursive;
-        }
-        
-        a.btns:hover{
-            /*transform: skew(-20deg);*/
-            background-color: #17ecf0;
-        }
-        
-        #divMap{
-            background-image: url("static/imgMapFtnt.png");
-            background-position: center;
-        }
-        
-        #divPouso{
-            background-image: url("static/imgpouso.jpeg");
-            background-position: center;
-        }
-        
-        #divPontosMap{
-            background-image: url("static/imgpontosdomapa.jpeg");
-            background-position: center;
-        }
-        
-        div#container{
-            margin: 5% 0;
-        }
-        
-        
-        p#pImg1{
-            background-image: url("static/fundo2p.png");
-            width: 100%;
-            height: 600px;
-        }
-        
-        .elementRight {
-            text-align: right;
-        }
-        
-    
-        
-        footer{
-            height: auto;
-            background-color: #5c149b;
-            box-shadow: 0 0 1em #222831;
-            margin: 0;
-            color: white; 
-        }
-        
-        footer p{
-            text-align: center;
-            padding: 2% 0;
-            font-size: 1.1em;
-        }
-        
-        nav#menuFooter ul li{
-            list-style: none;
-            font-family: 'Londrina Solid', cursive;
-            margin-bottom: 2px;
-        }
-        
-        nav#menuFooter{
-            font-family: 'Londrina Solid', cursive;
-            display: flex;
-            padding-top: 5%;
-        }
-        
-        .divFlexsFooter{
-            flex: 2;
-            text-align: center;
-        }
-        
-        nav#menuFooter ul li a{
-            text-decoration: none;
-            color: white;
-            font-size: 1.2em;
-        }
-        
-        nav#menuFooter ul li a:hover{
-            color: #17ecf0;
-            padding: 5px;
-        }
-        
-        div#logoFooter{
-           border-left: 1px solid #17ecf0;;
-        }
+       
         
         |]
         
@@ -216,15 +92,4 @@ getHomeR = do
                         <img src="imgs/imgMapFtnt.png" alt="Logo do site" height="150px" width="150px"> 
     
             <p>© 2019 - Review Game FTNT
-        <!-
-           $maybe nome <- sess
-                    <li>
-                        <div>
-                            Ola #{nome}
-                        <form method=post action=@{SairR}>
-                            <input type="submit" value="Sair">
-            $nothing
-                    <h1>
-                        CONVIDADO 
-                        ->
         |]
