9d7a7e3a72403dc9ba084264e691cddf9c066e0e
Ubuntu <ubuntu@ip-172-31-29-135.ec2.internal>
2019-12-02 03:12:24 +0000

    PÁGINA COMBAT

--- src/Handler/Characters.hs
+++ src/Handler/Characters.hs
@@ -43,6 +43,17 @@ getCharactersR = do
         
         <div id="container">
             <h2>Characters
+            <p>One of the biggest news of Chapter 2 is the presence of boats. In addition, players can fish and swim in the waters of the new map. With boats, you can move faster and shoot enemies. This will bring new game dynamics, with players being able to get around by air, soil and water.
+
+            <p id="pImg1">
+
+            <p class="elementRight">More aspects have been added to the gameplay. Now one player can carry the other down. This is for both friends to take them to safe places and enemies to do the opposite. The arsenal of weapons has been simplified, but it has also brought Bandage Bazooka, a weapon that fires a projectile capable of extending the life of its allies.
+
+            <p class="elementRight">Fortnite's new world is dramatically different from past seasons. This is mainly due to the role that water will play in the game. Rivers and lakes center and meet at the center of the map, bringing new ways to explore paths and locomotion to the safe zone.
+
+            <p class="elementRight">The biomes in the map regions are also very varied. Players will find places with coastal landscapes, mountains, lakes, farms, forests and swamps. There are many open and elevated spaces to explore, and this should have a direct impact on gameplay.
+            <a href="@{MapR}" class="btns">find your map
+            <a href="@{LocationsR}" class="btns">locations on the map
             
                 
         <footer>
